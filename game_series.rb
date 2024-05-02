3ab7faf8ff725602194a6c0d4ed26577451010e0
Craig Tingey <tingeycraig@gmail.com>
2024-05-01 20:53:50 -0600

    format game summary output for game series

--- game_series.rb
+++ game_series.rb
@@ -39,14 +39,16 @@ class GameSeries
   end
 
   def summary
-    sorted_players = stats.map do |name, data|
-      {player_name: name}.merge(data)
-    end.sort_by { |player| player[:win_rate] }.reverse
     @leader_board = sorted_players.each_with_index.map { |player, i| "#{i+1}: #{ player[:player_name] } - win_rate: " + "#{ player[:win_rate] }%".colorize(:light_blue) + " placements_score: " + "#{ player[:placement_score] }".colorize(:red) + " average_end_game_$$: " + "$#{ player[:avg_score] }".colorize(:green) + " total_money_won: " + "$#{ player[:total_money_won] }".colorize(:green) }
   end
 
   def print_summary
-    puts summary
+    sorted_players.each_with_index do |player, i|
+      printf "%-13s %-30s %-35s %-40s %-40s\n", "#{(i + 1)}: #{player[:player_name]}", "win rate: " + "#{player[:win_rate]}%".colorize(:light_blue), "PlacementScore " + "#{player[:placement_score]}".colorize(:red), "Avg. End Game $$: " + "$#{player[:avg_score]}".colorize(:green), "Total Money Won: " + "$#{player[:total_money_won]}".colorize(:green)
+    end
+    puts "\nAchievements:"
+    puts "Most Consistent Player: " + "#{most_consistent_player}".colorize(:light_blue)
+    puts "Richest Player: #{richest_player[:player_name]} with " + "$#{richest_player[:total_money_won]}".colorize(:green)
   end
 
   private
@@ -62,4 +64,18 @@ class GameSeries
     end
   end
 
+  def sorted_players
+    stats.map do |name, data|
+      {player_name: name}.merge(data)
+    end.sort_by { |player| player[:win_rate] }.reverse
+  end
+
+  def most_consistent_player
+    player = sorted_players.max_by { |player| player[:placement_score] }
+    player[:player_name]
+  end
+
+  def richest_player
+    sorted_players.max_by { |player| player[:total_money_won] }
+  end
 end
